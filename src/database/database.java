81a42b1b8897e8db73220a3e8fed7bd2705c32da
“ssohye” <ssohye@icloud.com>
2022-11-12 18:21:59 +0900

    newroom function added

--- src/database/database.java
+++ src/database/database.java
@@ -1,6 +1,9 @@
 package database;
 
 import java.sql.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class database {
     String url = "jdbc:mysql://swiftsjh.tplinkdns.com:3306/insta";
@@ -62,4 +65,49 @@ public class database {
 
 
     }
+
+    public int newroom(HashSet<String> member){
+
+        int chat_id=0;
+        String getlastchatnum="SELECT chat_id FROM chat_manager ORDER BY chat_id DESC LIMIT 1;";
+
+        try {
+            result = statement.executeQuery(getlastchatnum);
+            while(result.next()) {
+                chat_id = Integer.parseInt(result.getString(1));
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        chat_id+=1;
+
+        Iterator iter = member.iterator();	// Iterator 사용
+        while(iter.hasNext()) {
+            String sq = "insert into chat_manager values(?,?)";
+
+            try {
+                preparedstatement = con.prepareStatement(sq);
+                preparedstatement.setString(1, Integer.toString(chat_id));
+                preparedstatement.setString(2, iter.next().toString());
+                int count = preparedstatement.executeUpdate();
+                if (count == 0) {
+                    System.out.println("데이터 입력 실패");
+
+                } else {
+                    System.out.println("데이터 입력 성공");
+
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+
+
+
+        return chat_id;
+    }
+
+
+
 }
