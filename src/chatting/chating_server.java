d9702a4cb9c24c913e1c31157b21866750d8f46e
“ssohye” <ssohye@icloud.com>
2022-11-24 04:32:39 +0900

    채팅서버 개발

--- src/chatting/chating_server.java
+++ src/chatting/chating_server.java
@@ -157,7 +157,7 @@ public class chating_server implements Runnable {
                             }
                         }else{
 
-                            System.out.println("이미 연결정보에 등록됨");
+                            System.out.println("새로 연결정보에 등록됨");
                             for(int i=0;i<connection_list.size();i++){
                                 if(connection_list.get(i).room_id.equals(room_id)&&connection_list.get(i).user_id!=user_id){
                                     System.out.println("방에 있는 사람들에게 메세지 전송");
