696975e80dd912b6e53ef0e52d20bd717eeb605b
“ssohye” <ssohye@icloud.com>
2022-11-24 02:26:50 +0900

    채팅서버 개발

--- src/chatting/chating_server.java
+++ src/chatting/chating_server.java
@@ -188,7 +188,7 @@ public class chating_server implements Runnable {
                              }
                          }
                      } else if (content.getTypeofrequest() == 5) { //방목록 업데이트 요청인경우
-                         System.out.println("방목록 업데이트 요청이 들어옴"+  content.getSender());
+                         System.out.println("방목록 업데이트 요청이 들어옴" + content.getSender());
                          int update_user_id = content.getSender();
                          ArrayList<String> room_list = db.get_room_list(update_user_id);
                          protocol response = new protocol(6, room_list);
@@ -199,6 +199,7 @@ public class chating_server implements Runnable {
                      } else {
                          System.out.println("잘못된 요청입니다.");
                      }
+                     is = socket.getInputStream();
                      ois= new ObjectInputStream(is);
                  }
                      ois.close();
